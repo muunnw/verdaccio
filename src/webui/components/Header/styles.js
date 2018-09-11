e3e74f065259f4301fb4069e3a7fae3d5552c575
Priscila Oliveira <priscilawebdev@gmail.com>
2018-09-11 23:02:38 +0200

    feat: added media sizes

--- src/webui/components/Header/styles.js
+++ src/webui/components/Header/styles.js
@@ -1,12 +1,14 @@
 
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import colors from '../../utils/styles/colors';
+import mq from '../../utils/styles/media';
 
 export const Wrapper = styled(AppBar)`
     && {
         background-color: ${colors.primary};
+        position: fixed;
     }
 `;
 
@@ -14,5 +16,15 @@ export const InnerWrapper = styled(Toolbar)`
     && {
         justify-content: space-between;
         align-items: center;
+        padding: 0 20px;
+        ${mq.medium(css`
+            min-width: 400px;
+            max-width: 800px;
+            width: 100%;
+            margin: auto;
+        `)};
+        ${mq.large(css`
+            max-width: 1240px;
+        `)};
     }
 `;
